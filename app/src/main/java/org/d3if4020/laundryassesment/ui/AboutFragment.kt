6cc6e23569620ac62807b93a248d561b39e832c0
ummam <ummam1227@gmail.com>
2021-04-01 12:05:24 +0700

    menambahkan fragment about, navigasi button dan up, menambahkan share data ke aplikasi lain

---
+++ app/src/main/java/org/d3if4020/laundryassesment/ui/AboutFragment.kt
@@ -0,0 +1,7 @@
+package org.d3if4020.laundryassesment.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4020.laundryassesment.R
+
+class AboutFragment  : Fragment(R.layout.fragment_about){
+}
--- app/src/main/java/org/d3if4020/laundryassesment/ui/Awal.kt
+++ app/src/main/java/org/d3if4020/laundryassesment/ui/Awal.kt
@@ -1,12 +1,13 @@
 package org.d3if4020.laundryassesment.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if4020.laundryassesment.R
 import org.d3if4020.laundryassesment.databinding.FragmentAwalBinding
 
@@ -17,8 +18,26 @@ class Awal : Fragment() {
         binding = FragmentAwalBinding.inflate(
                 layoutInflater, container, false)
         binding.button.setOnClickListener { hitungHarga() }
-        binding.hasilTextView.text = getString(R.string.TotalHarga)
+        binding.lanjutButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                R.id.action_awal_to_saranFragment
+            )
+        }
+        binding.shareButton.setOnClickListener { shareData() }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_awal_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
     private fun hitungHarga() {
         val bulanan = binding.paketEditText.text.toString()
@@ -30,5 +49,19 @@ class Awal : Fragment() {
 
         binding.hasilTextView.text = getString(R.string.TotalHarga)
         binding.laundryTextView.text = hasil.toString()
+        binding.buttonGroup.visibility = View.VISIBLE
     }
+    private fun shareData() {
+        val message = getString(R.string.bagikan_template,
+            binding.paketEditText.text,
+            binding.laundryTextView.text
+        )
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager) != null) {
+            startActivity(shareIntent)
+        }
+    }
+
 }
